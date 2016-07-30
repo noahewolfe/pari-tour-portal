9c76e6483c0ccbcf4ba4674c38f12c62e527c1f4
Noah Wolfe <Noah Wolfe>
2016-07-30 10:57:31 -0400

    Updated local models, added ability to updated database from web form

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+// libraries
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// local configuration
+var configDB = require('./config/database.js');
+
+// db models
+var Place = require('./models/place.js');
 
 var PORT = 8080;
+
+// configuration ================================
+// setup mongo db
+mongoose.connect(configDB.url);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -10,6 +26,11 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
+app.post('/', function (req, res) {
+    console.dir(req.body);
+    var newPlace = new Place(req.body);
+    newPlace.save();
+})
 
 // listener
 app.listen(PORT, function () {
--- models/image.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-// define the schema for our user model
-var imageSchema = mongoose.Schema({
-    ref          : String //make sure these are always unique!
-});
-
-//creating the model and exposing it to the app
-module.exports = mongoose.model('DB_NAME', imageSchema);
